74f335b3711943a04c34b5441229f974db0e52fc
Max Frontend <maxfarseer@gmail.com>
2020-02-16 10:13:44 +0100

    render fruit list

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,42 @@
 module Main exposing (main)
 
-import Browser
-import Html exposing (h1, text)
+import Html exposing (Html, div, h1, li, text, ul)
+
+
+
+-- переменные (аналог const XXX = ... )
+
+
+fruits : List String
+fruits =
+    [ "яблоко", "банан", "груша" ]
+
+
+
+-- функции для отрисовки
+
+
+renderItem : String -> Html msg
+renderItem fruitName =
+    li [] [ text fruitName ]
+
+
+renderFruits : List String -> Html msg
+renderFruits data =
+    let
+        list =
+            List.map renderItem data
+    in
+    ul [] list
+
+
+
+-- главная view функция
+
 
 main =
-  h1 [] [ text "Hello, Elm!" ]
+    div []
+        [ h1 []
+            [ text "Сезон фруктов!" ]
+        , renderFruits fruits
+        ]
